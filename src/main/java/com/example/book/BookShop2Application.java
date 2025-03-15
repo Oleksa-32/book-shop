18d279770979a912051938ca494b666b8f3a7381
user <graban2253@gmail.com>
2025-03-15 13:32:13 +0200

    Initialized project

---
+++ src/main/java/com/example/book/BookShop2Application.java
@@ -0,0 +1,13 @@
+package com.example.book;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookShop2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookShop2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/book/BookShop2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.book;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookShop2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
