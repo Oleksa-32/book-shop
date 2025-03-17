44fad92bebce38d211eafeabc66bed1414221f88
user <graban2253@gmail.com>
2025-03-17 13:11:20 +0200

    refactored

--- src/main/java/com/example/book/service/BookServiceImpl.java
+++ src/main/java/com/example/book/service/BookServiceImpl.java
@@ -3,9 +3,7 @@ package com.example.book.service;
 import com.example.book.model.Book;
 import com.example.book.repository.BookRepository;
 import java.util.List;
-
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
