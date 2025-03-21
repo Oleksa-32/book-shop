b0d3f3cb7713a7d9341cb04aa278a2107e1d917d
user <graban2253@gmail.com>
2025-03-22 00:26:17 +0200

    added GlobalExceptionHandler and data validation

--- src/main/java/com/example/book/dto/CreateBookRequestDto.java
+++ src/main/java/com/example/book/dto/CreateBookRequestDto.java
@@ -1,16 +1,27 @@
 package com.example.book.dto;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.example.book.validation.Path;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
 import java.math.BigDecimal;
 import lombok.Data;
+import lombok.NonNull;
+import org.hibernate.validator.constraints.ISBN;
 
 @Data
-@JsonIgnoreProperties(ignoreUnknown = true)
 public class CreateBookRequestDto {
+    @NonNull
+    @Size(max = 100, message = "Title length must be less then 100 characters")
     private String title;
+    @NonNull
     private String author;
+    @NonNull
+    @ISBN
     private String isbn;
+    @NonNull
+    @Positive
     private BigDecimal price;
     private String description;
+    @Path
     private String coverImage;
 }
