471e3aaa2be7d7131ac7428e5ae7ecdf42e41450
user <graban2253@gmail.com>
2025-03-21 20:26:14 +0200

    refactored

--- src/main/java/com/example/book/model/Book.java
+++ src/main/java/com/example/book/model/Book.java
@@ -10,14 +10,14 @@ import java.math.BigDecimal;
 import lombok.Getter;
 import lombok.Setter;
 import org.hibernate.annotations.SQLDelete;
-import org.hibernate.annotations.Where;
+import org.hibernate.annotations.SQLRestriction;
 
 @Entity
 @Table(name = "books")
 @Getter
 @Setter
 @SQLDelete(sql = "UPDATE books SET is_deleted = true WHERE id=?")
-@Where(clause = "is_deleted=false")
+@SQLRestriction("status <> 'DELETED'")
 public class Book {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
