4615ca3870dc14843b74322ece33b43ebf2c6f1f
user <graban2253@gmail.com>
2025-03-29 16:26:55 +0200

    refactored

--- src/main/java/com/example/book/security/CustomUserDetailsService.java
+++ src/main/java/com/example/book/security/CustomUserDetailsService.java
@@ -1,6 +1,7 @@
 package com.example.book.security;
 
 import com.example.book.repository.UserRepository;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -15,7 +16,7 @@ public class CustomUserDetailsService implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
         return userRepository.findByEmail(email)
-                .orElseThrow(() -> new UsernameNotFoundException("Can't find user by email"));
+                .orElseThrow(() -> new EntityNotFoundException("Can't find user by email"));
 
     }
 }
